886cccea4780b48935fdd495b615b8876896b1ee
leobuck <leobuck12@gmail.com>
2023-08-07 19:56:21 -0300

    Adicionando telefones

---
+++ src/main/java/escola/Telefone.java
@@ -0,0 +1,14 @@
+package escola;
+
+public class Telefone {
+
+	private String ddd;
+	private String numero;
+	
+	public Telefone(String ddd, String numero) {
+		super();
+		this.ddd = ddd;
+		this.numero = numero;
+	}
+	
+}
