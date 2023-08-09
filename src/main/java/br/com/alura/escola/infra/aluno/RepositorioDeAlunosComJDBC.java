37deb27101b77eedfe7a9093118245a465bd7c4a
leobuck <leobuck12@gmail.com>
2023-08-09 11:55:19 -0300

    Implementando em memória

--- src/main/java/br/com/alura/escola/infra/aluno/RepositorioDeAlunosComJDBC.java
+++ src/main/java/br/com/alura/escola/infra/aluno/RepositorioDeAlunosComJDBC.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.alura.escola.dominio.aluno.Aluno;
+import br.com.alura.escola.dominio.aluno.AlunoNaoEncontrado;
 import br.com.alura.escola.dominio.aluno.CPF;
 import br.com.alura.escola.dominio.aluno.FabricaDeAluno;
 import br.com.alura.escola.dominio.aluno.RepositorioDeAlunos;
@@ -47,19 +48,31 @@ public class RepositorioDeAlunosComJDBC implements RepositorioDeAlunos {
 	@Override
 	public Aluno buscarPorCPF(CPF cpf) {
 		try {
-			Aluno aluno = null;
-			String sql = "SELECT * FROM aluno WHERE cpf = ?";
+			String sql = "SELECT id, nome, email FROM aluno WHERE cpf = ?";
 			PreparedStatement ps = connection.prepareStatement(sql);
 			ps.setString(1, cpf.getNumero());
-			ResultSet rs = ps.executeQuery();
 			
+			ResultSet rs = ps.executeQuery();
+			boolean encontrou = rs.next();
+			if (!encontrou) {
+				throw new AlunoNaoEncontrado(cpf);
+			}
+			
+			String nome = rs.getString("nome");
+			String email = rs.getString("email");
+			Aluno aluno = new FabricaDeAluno()
+					.comNomeCPFEmail(nome, cpf.getNumero(), email)
+					.criar();
+			
+			Long id = rs.getLong("id");
+			sql = "SELECT ddd, numero FROM telefone WHERE aluno_id = ?";
+			ps = connection.prepareStatement(sql);
+			ps.setLong(1, id);
+			rs = ps.executeQuery();
 			while (rs.next()) {
-				String nome = rs.getString("nome");
-				String cpfAluno = rs.getString("cpf");
-				String email = rs.getString("email");
-				aluno = new FabricaDeAluno()
-						.comNomeCPFEmail(nome, cpfAluno, email)
-						.criar();
+				String ddd = rs.getString("ddd");
+				String numero = rs.getString("numero");
+				aluno.adicionarTelefone(ddd, numero);
 			}
 			
 			return aluno;
@@ -73,7 +86,7 @@ public class RepositorioDeAlunosComJDBC implements RepositorioDeAlunos {
 	public List<Aluno> listarTodosAlunosMatriculados() {
 		try {
 			List<Aluno> alunos = new ArrayList<>();
-			String sql = "SELECT * FROM aluno";
+			String sql = "SELECT id, cpf, nome, email FROM aluno";
 			PreparedStatement ps = connection.prepareStatement(sql);
 			ResultSet rs = ps.executeQuery();
 			
@@ -84,6 +97,17 @@ public class RepositorioDeAlunosComJDBC implements RepositorioDeAlunos {
 				Aluno aluno = new FabricaDeAluno()
 						.comNomeCPFEmail(nome, cpf, email)
 						.criar();
+				
+				Long id = rs.getLong("id");
+				sql = "SELECT ddd, numero FROM telefone WHERE aluno_id = ?";
+				PreparedStatement psTelefone = connection.prepareStatement(sql);
+				psTelefone.setLong(1, id);
+				ResultSet rsTelefone = psTelefone.executeQuery();
+				while (rsTelefone.next()) {
+					String ddd = rsTelefone.getString("ddd");
+					String numero = rsTelefone.getString("numero");
+					aluno.adicionarTelefone(ddd, numero);
+				}
 				
 				alunos.add(aluno);
 			}
