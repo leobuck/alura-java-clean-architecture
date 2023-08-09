4295dec3becad6c872a84f0318a40443e5dedc43
leobuck <leobuck12@gmail.com>
2023-08-09 11:28:23 -0300

    Implementação dos métodos de busca

--- src/main/java/br/com/alura/escola/infra/aluno/RepositorioDeAlunosComJDBC.java
+++ src/main/java/br/com/alura/escola/infra/aluno/RepositorioDeAlunosComJDBC.java
@@ -2,11 +2,14 @@ package br.com.alura.escola.infra.aluno;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import br.com.alura.escola.dominio.aluno.Aluno;
 import br.com.alura.escola.dominio.aluno.CPF;
+import br.com.alura.escola.dominio.aluno.FabricaDeAluno;
 import br.com.alura.escola.dominio.aluno.RepositorioDeAlunos;
 import br.com.alura.escola.dominio.aluno.Telefone;
 
@@ -43,14 +46,52 @@ public class RepositorioDeAlunosComJDBC implements RepositorioDeAlunos {
 
 	@Override
 	public Aluno buscarPorCPF(CPF cpf) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			Aluno aluno = null;
+			String sql = "SELECT * FROM aluno WHERE cpf = ?";
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ps.setString(1, cpf.getNumero());
+			ResultSet rs = ps.executeQuery();
+			
+			while (rs.next()) {
+				String nome = rs.getString("nome");
+				String cpfAluno = rs.getString("cpf");
+				String email = rs.getString("email");
+				aluno = new FabricaDeAluno()
+						.comNomeCPFEmail(nome, cpfAluno, email)
+						.criar();
+			}
+			
+			return aluno;
+			
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	@Override
 	public List<Aluno> listarTodosAlunosMatriculados() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			List<Aluno> alunos = new ArrayList<>();
+			String sql = "SELECT * FROM aluno";
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			
+			while (rs.next()) {
+				String nome = rs.getString("nome");
+				String cpf = rs.getString("cpf");
+				String email = rs.getString("email");
+				Aluno aluno = new FabricaDeAluno()
+						.comNomeCPFEmail(nome, cpf, email)
+						.criar();
+				
+				alunos.add(aluno);
+			}
+			
+			return alunos;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
