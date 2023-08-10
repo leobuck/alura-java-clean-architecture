674371d1fc1f202a6c32833c4e16acff7826e8b0
leobuck <leobuck12@gmail.com>
2023-08-10 15:12:01 -0300

    Testando o Use Case

---
+++ src/test/java/br/com/alura/escola/aplicacao/aluno/matricular/MatricularAlunoTest.java
@@ -0,0 +1,28 @@
+package br.com.alura.escola.aplicacao.aluno.matricular;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import br.com.alura.escola.dominio.aluno.Aluno;
+import br.com.alura.escola.dominio.aluno.CPF;
+import br.com.alura.escola.infra.aluno.RepositorioDeAlunosEmMemoria;
+
+class MatricularAlunoTest {
+
+	@Test
+	void alunoDeveriaSerPersistido() {
+		RepositorioDeAlunosEmMemoria repositorio = new RepositorioDeAlunosEmMemoria();
+		MatricularAluno useCase = new MatricularAluno(repositorio);
+		
+		MatricularAlunoDto dados = new MatricularAlunoDto("Fulano", "123.456.789-00", "fulano@email.com");
+		useCase.executar(dados);
+		
+		Aluno encontrado = repositorio.buscarPorCPF(new CPF("123.456.789-00"));
+		
+		assertEquals("Fulano", encontrado.getNome());
+		assertEquals("123.456.789-00", encontrado.getCpf());
+		assertEquals("fulano@email.com", encontrado.getEmail());
+	}
+
+}
