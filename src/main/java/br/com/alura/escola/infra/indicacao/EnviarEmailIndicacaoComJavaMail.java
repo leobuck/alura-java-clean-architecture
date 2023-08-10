9c9920b2ca076cb0e07657e471b56e7725f7122c
leobuck <leobuck12@gmail.com>
2023-08-10 12:22:37 -0300

    Implementação do envio de e-mail com JavaMail

--- src/main/java/br/com/alura/escola/infra/indicacao/EnviarEmailIndicacaoComJavaMail.java
+++ src/main/java/br/com/alura/escola/infra/indicacao/EnviarEmailIndicacaoComJavaMail.java
@@ -1,5 +1,19 @@
 package br.com.alura.escola.infra.indicacao;
 
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
+import javax.mail.Address;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.Message.RecipientType;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import br.com.alura.escola.aplicacao.indicacao.EnviarEmailIndicacao;
 import br.com.alura.escola.dominio.aluno.Aluno;
 
@@ -7,7 +21,35 @@ public class EnviarEmailIndicacaoComJavaMail implements EnviarEmailIndicacao {
 
 	@Override
 	public void enviarPara(Aluno indicado) {
-		// logica de envio de email com a lib Java Mail
+		Properties properties = new Properties();
+		properties.put("mail.smtp.ssl.trust", "*");
+		properties.put("mail.smtp.auth", "true");
+		properties.put("mail.smtp.starttls", "true");
+		properties.put("mail.smtp.host", "smtp.gmail.com");
+		properties.put("mail.smtp.port", "465");
+		properties.put("mail.smtp.socketFactory.port", "465");
+		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		
+		Session session = Session.getInstance(properties, new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("", "");
+			}
+		});
+		
+		try {
+			Address[] dest = InternetAddress.parse(indicado.getEmail());
+			
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("", ""));
+			message.setRecipients(RecipientType.TO, dest);
+			message.setSubject("Bem-vindo");
+			message.setContent("Seja bem-vindo!", "text/html; charset=utf-8");
+			
+			Transport.send(message);
+		} catch (UnsupportedEncodingException | MessagingException e) {
+			throw new RuntimeException("Erro ao enviar o e-mail de indicação");
+		}
 	}
 
 }
